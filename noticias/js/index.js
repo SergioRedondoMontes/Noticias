4b83eacaef0a03275d018e741d6b0336c8a0c764
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2019-12-02 10:32:46 +0100

    add modal to index.html & create funct modal

--- noticias/js/index.js
+++ noticias/js/index.js
@@ -1,39 +1,38 @@
+let $table = $("#table");
+let $remove = $("#remove");
+var selectedRow = null;
+var mapNoticias = null;
 
-
-let $table = $('#table')
-let $remove = $('#remove')
-var selectedRow = null
-var mapNoticias = null
-
-$( $table ).ready(function() {
-    getNews();
+$($table).ready(function() {
+  getNews();
 });
 
-function getNews(){
-    $.get("http://127.0.0.1:8000/api/noticia/", data => {
-        if(data != []){
-            //   for (i = 0; i < data.length; i++) {
-            //       console.log(data[i]);
-            //   }
-            orderNews(data);
-          }else{
-              //TODO: Delete table
-          }
-          })
-      }
-
-// ordena por fecha las noticias
-function orderNews(data){
- 
-    //"05-11-2019 22:20:44"
-    orderNoticias = data.sort(function(a,b){
-        // Turn your strings into dates, and then subtract them
-        // to get a value that is either negative, positive, or zero.
-        return dateFormat(new Date(b.pub_date), "dd-mm-yyyy H:MM:ss") -  dateFormat(new Date(a.pub_date), "dddd, mmmm dS, yyyy, h:MM:ss TT");
-      });
-    insertNews();
+function getNews() {
+  $.get("http://127.0.0.1:8000/api/noticia/", data => {
+    if (data != []) {
+      //   for (i = 0; i < data.length; i++) {
+      //       console.log(data[i]);
+      //   }
+      orderNews(data);
+    } else {
+      //TODO: Delete table
+    }
+  });
 }
 
+// ordena por fecha las noticias
+function orderNews(data) {
+  //"05-11-2019 22:20:44"
+  orderNoticias = data.sort(function(a, b) {
+    // Turn your strings into dates, and then subtract them
+    // to get a value that is either negative, positive, or zero.
+    return (
+      dateFormat(new Date(b.pub_date), "dd-mm-yyyy H:MM:ss") -
+      dateFormat(new Date(a.pub_date), "dddd, mmmm dS, yyyy, h:MM:ss TT")
+    );
+  });
+  insertNews();
+}
 
 /**Futuras modificaciones
  * <div class="card" style="width: 18rem;">
@@ -44,60 +43,70 @@ function orderNews(data){
 </div>
  */
 
- function insertNews(){
-    const news = $('#news');
-    for (i = 0; i < 10; i++) {
-        if(i === 0){
-            var firstCol =$('<div>', {
-                'class': 'col-12 border border-primary',
-                'id': i
-            })
-            var row = $('<div>', {
-                'class': 'row',
-            })
-            var col = $('<div>', {
-                'class': 'd-flex col-12 justify-content-center align-content-center'
-            })
-            col.append($('<h2>'+ orderNoticias[i].title +'</h2>'))
-            var col1 = $('<div>', {
-                'class': 'col-6'
-            })
-            col1.append($('<img src=' + orderNoticias[i].img_url + '></img>'))
-            var col2 = $('<div>', {
-                'class': 'd-flex col-6'
-            })
-            col2.append($('<h4>'+ orderNoticias[i].header +'</h4>'))
-            var button = $('<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">Leer mas...</button>')
+function insertNews() {
+  const news = $("#news");
+  for (i = 0; i < 10; i++) {
+    if (i === 0) {
+      var firstCol = $("<div>", {
+        class: "col-12 border border-primary",
+        id: i
+      });
+      var row = $("<div>", {
+        class: "row"
+      });
+      var col = $("<div>", {
+        class: "d-flex col-12 justify-content-center align-content-center"
+      });
+      col.append($("<h2>" + orderNoticias[i].title + "</h2>"));
+      var col1 = $("<div>", {
+        class: "col-6"
+      });
+      col1.append($("<img src=" + orderNoticias[i].img_url + "></img>"));
+      var col2 = $("<div>", {
+        class: "d-flex col-6"
+      });
+      col2.append($("<h4>" + orderNoticias[i].header + "</h4>"));
+      var button = $(
+        '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">Leer mas...</button>'
+      );
 
-            col2.append(button);
+      col2.append(button);
 
-            row.append(col);
-            row.append(col1);
-            row.append(col2);
+      row.append(col);
+      row.append(col1);
+      row.append(col2);
 
-            firstCol.append(row);
+      firstCol.append(row);
 
-            news.append(firstCol);
-        }else{
-            var firstCol =$('<div>', {
-                'class': 'col-4',
-                'id': i
-            })
+      news.append(firstCol);
+    } else {
+      var firstCol = $("<div>", {
+        class: "col-4",
+        id: i
+      });
 
-            var card = $('<div class="card">')
-            var img = $('<img src="'+orderNoticias[i].img_url+'" class="card-img-top" alt="...">')
-            var cardbody = $('<div class="card-body">')
-            var title = $('<p class="card-title">'+ orderNoticias[i].title +'</p>');
-            var button = $('<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">Leer mas...</button>')
+      var card = $('<div class="card">');
+      var img = $(
+        '<img src="' +
+          orderNoticias[i].img_url +
+          '" class="card-img-top" alt="...">'
+      );
+      var cardbody = $('<div class="card-body">');
+      var title = $('<p class="card-title">' + orderNoticias[i].title + "</p>");
+      var button = $(
+        '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">Leer mas...</button>'
+      );
 
-            cardbody.append(title);
-            cardbody.append(button);
-            card.append(img);
-            card.append(cardbody);
-            
-            firstCol.append(card);
+      cardbody.append(title);
+      cardbody.append(button);
+      card.append(img);
+      card.append(cardbody);
 
-            news.append(firstCol);
-        }
+      firstCol.append(card);
+
+      news.append(firstCol);
     }
- }
+  }
+}
+
+function createModal() {}
